29a30f6fb955de518d94d1e236a7c41d1570a3e4
mikus-loli <18726963417@qq.com>
2026-03-02 19:34:04 +0800

    fix(音乐播放器): 将默认循环模式改为列表循环

--- src/components/widget/MusicPlayer.svelte
+++ src/components/widget/MusicPlayer.svelte
@@ -46,7 +46,7 @@ let isLoading = false;
 // 是否随机播放，默认为 false
 let isShuffled = false;
 // 循环模式，0: 不循环, 1: 单曲循环, 2: 列表循环，默认为 0
-let isRepeating = 0;
+let isRepeating = 2;
 // 错误信息，默认为空字符串
 let errorMessage = "";
 // 是否显示错误信息，默认为 false
