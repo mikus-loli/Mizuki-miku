baeca02d516e2f16759dc434b86b3ee8e583109d
mikus-loli <18726963417@qq.com>
2026-06-05 20:19:32 +0800

    fix(config): 修改主页横幅主标题为Mikus Blog
    将站点主页的横幅主标题从"Mikus"调整为"Mikus Blog"，优化站点标题展示效果

--- src/config.ts
+++ src/config.ts
@@ -163,7 +163,7 @@ export const siteConfig: SiteConfig = {
 
 		homeText: {
 			enable: true, // 在主页显示自定义文本
-			title: "Mikus", // 主页横幅主标题
+			title: "Mikus Blog", // 主页横幅主标题
 
 			subtitle: [
 				"最喜欢Miku了",
